0f564f6203078c779a67b2d889bb94078141bf13
nthend <nthend.iipa@gmail.com>
2014-09-04 21:55:54 +0700

    open course page in new window

--- schedule/course.js
+++ schedule/course.js
@@ -28,7 +28,8 @@ Course.create = function (start,length,type,name,place,teacher)
 	course.innerElement = document.createElement('div');
 	course.innerElement.setAttribute('class','course');
 	course.innerElement.href = course.name.link;
-	course.innerElement.onclick = function () {window.location.href = this.href;};
+	course.innerElement.onclick = function () { window.open(this.href); };
+	course.innerElement.onmousedown = function (event) { if(event.which == 2) { window.open(this.href, '_blank'); } };
 	course.element.appendChild(course.innerElement);
 	
 	course.slider = new Object();
